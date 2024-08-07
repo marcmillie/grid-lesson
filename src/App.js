80e78c3db978a702837411c641a98eefe9634c12
marcmillie <mrmarcusmiller0@gmail.com>
2024-08-07 00:57:25 -0400

    added base elements and tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div class="container">
+        <header>
+          <h1>Header</h1>
+        </header>
+        <nav>
+          <h1>Nav</h1>
+        </nav>
+        <section class="left">
+          <h1>Left</h1>
+        </section>
+        <section class="right">
+          <h1>Right</h1>
+        </section>
+        <footer>
+          <h1>Footer</h1>
+        </footer>
+      </div>
     </div>
   );
 }
